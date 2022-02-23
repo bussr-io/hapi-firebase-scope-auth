3a447bc2eaad74878a87375649e6649d9fd83b92
Bharath Kontham <bharath2211@gmail.com>
2022-02-23 15:32:21 +0530

    add plugin and readme

---
+++ README.md
@@ -0,0 +1,65 @@
+# hapi-firebase-scope-auth
+
+## Intro & Features
+
+* Firebase Authentication plugin for HapiJS.
+* This is used for validating Firebase authentication for HapiJs routes.
+* Has support for Role based Access Control (RBAC) with firebase for API routes
+
+## Installation
+
+> npm install hapi-firebase-scope-auth
+
+## Usage
+
+```javascript
+ 
+// Plugin registration
+await server.register(require('hapi-firebase-auth'))
+
+// Initialize Firebase Admin SDK
+
+const { initializeApp } = require('firebase-admin/app')
+const { getAuth } = require('firebase-admin/auth')
+
+const fireApp = initializeApp()
+const firebaseAdminAuthInstance = getAuth(fireApp)
+ 
+// Create strategy
+server.auth.strategy('myFirebase', 'firebase-scope', {
+  firebaseAdminAuth: firebaseAdminAuthInstance,
+  scopeField: 'role'
+})
+
+```
+
+## RBAC Usage
+
+* This plugin uses Firebase Admin SDK's verifyIdToken method to validate token.
+* Once the token is validated then verifyIdToken returns JWT decoded values which include user details and custom claims.
+* Set *scopeField* value in strategy as shown  in above configuration. *scopeField* is any key / field from decoded JWT.
+* Based on scope configuration in route, it will be validated against *scopeField*
+
+```javascript
+
+// Following route will be authenticated if role value of user is either admin or author
+server.routes([{
+  method: 'GET',
+  path: '/health',
+  options: {
+    auth: {
+      strategy: 'myFirebase',
+      scope: ['admin', 'author']
+    },
+    tags: ['api', 'health'],
+    handler: LoginHandler,
+    plugins: {},
+    security: true
+  }
+}])
+
+```
+
+## Credits
+
+* Credits to Author of [hapi-firebase-auth](https://www.npmjs.com/package/hapi-firebase-auth) plugin.
---
+++ index.js
@@ -0,0 +1,79 @@
+const Boom = require('@hapi/boom')
+const Package = require('./package.json')
+
+const AuthenticationSchema = (server, options) => {
+  return {
+
+    /**
+     * Hapi's authenticate method
+     *
+     * @param {*} request Request object created by the server
+     * @param {*} h Standard hapi response toolkit
+     */
+    authenticate(request, h) {
+
+      // throw error if firbase admin auth instance is not configured
+      if (!options.firebaseAdminAuth) {
+        throw Boom.badRequest('Firebase Admin Auth Instance not configured')
+      }
+
+      // Get token from header
+      const Authorization = request.headers.authorization
+      if (!Authorization) return null
+
+      const bearerRegex = new RegExp(/(bearer )/i)
+      const Token = Authorization.replace(bearerRegex, '')
+
+      // If token not found, return an 'unauthorized' response
+      if (Token === null) {
+        throw Boom.unauthorized('Unauthorized')
+      }
+
+      // Validate token
+      return validate(Token, options.firebaseAdminAuth, h, options.scopeField)
+    }
+  }
+}
+
+
+/**
+ * Firebase User Token validation using Firebase Admin SDK
+ *
+ * @param {string} token Access token provided by Firebase Auth
+ * @param {*} firebaseAdminAuthInstance Initialized Firebase App instance
+ * @param {*} h Standard hapi response toolkit
+ * @param {string} scopeField field name from verifyIdToken which will be used for scope validation
+ */
+const validate = async (token, firebaseAdminAuthInstance, h, scopeField = 'role') => {
+  try {
+    const credentials = await firebaseAdminAuthInstance.verifyIdToken(token, true)
+    credentials.scope = credentials[scopeField] || ''
+    return h.authenticated({ credentials })
+  } catch (error) {
+    throw Boom.unauthorized('Invalid token')
+  }
+}
+
+/**
+ * Hapi Plugin register method
+ * @param {*} server 
+ * @param {*} options 
+ */
+const register = async (server, options) => {
+  server.auth.scheme('firebase-scope', AuthenticationSchema)
+
+  server.ext('onPostAuth', (request, h) => {
+    // TBD: Add any logic postAuth
+    return h.continue
+  })
+}
+
+// Export Plugin
+module.exports = {
+  plugin: {
+    name: Package.name,
+    version: Package.version,
+    pkg: Package,
+    register
+  }
+}
